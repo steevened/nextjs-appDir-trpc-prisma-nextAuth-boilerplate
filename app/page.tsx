9de676e39516889d61d3ac0d2612cb83575d34a2
steevened <alvaradosteven315@gmail.com>
2023-10-14 23:08:10 -0500

    add toggle theme buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+import { useTheme } from "next-themes";
+
 const Home = () => {
+  const { setTheme } = useTheme();
   return (
     <div>
+      <div className="space-x-2">
+        <button onClick={() => setTheme("light")}>light</button>
+        <button onClick={() => setTheme("dark")}>dark</button>
+      </div>
       <p>
         NEXT JS APP DIRECTORY - TRPC - PRISMA ORM - TAILWIND CSS - SHADCN UI -
         NEXT AUTH - NEXT THEMES
